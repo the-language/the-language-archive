a779fc45544a89a0002277b53d4a3bc33ae11e6d
zaoqi <zaomir@outlook.com>
2018-03-03 12:53:17 +0800

    init racket

---
+++ racket/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "lang")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("lang.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(zaoqi))
