0e9c21502ba83479609e1f9e433074f99c893197
zaoqi <zaomir@outlook.com>
2018-03-23 20:53:26 +0800

    .

--- racket/構.rkt
+++ racket/構.rkt
@@ -15,7 +15,18 @@
 ;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #lang racket
 (require srfi/9)
-(define (id x) x)
+(provide
+ 名/文？ 名/文 名/文-1 名/構？ 名/構 名/構.名 名/構.列 名？
+ 等？
+ 首-尾 首-尾？ 首-尾.首 首-尾.尾 空 空？
+ 文？
+ 映？ 映/空 映.增-改 映.取 映.含？ 映.删 映→列
+ 機 機？ #%機!境 機.形 #%機!物
+ 陰 楊 #%若
+ 引-機 引-機？ 引-機-1
+ 誤 誤？ 誤-1
+ 構 構？ 構.名 構.列
+ )
 
 (define 等？ equal?) ; BUG
 
@@ -26,60 +37,71 @@
 (define 空？ null?)
 (define 空 '())
 
-(define 字？ char?)
-
-(define-values (名？ 列→名 名→列)
-  ((λ ()
-     (define-custom-hash-types mh 等？)
-     (define h-un (make-mutable-mh))
-     (define h (make-mutable-mh))
-     (define c 0)
-     (define (名？ 甲) (dict-has-key? h 甲))
-     (define (列→名 列)
-       (dict-ref
-        h 列
-        (λ ()
-          (let ([s (string->symbol (number->string c))])
-            (set! c (+ c 1))
-            (dict-set! h-un s 列)
-            (dict-set! h 列 s)
-            s))))
-     (define (名→列 名) (dict-ref h-un 名))
-     (values 名？ 列→名 名→列))))
+(define 文？ char?)
 
 (define-custom-hash-types 映 等？)
 (define 映？ immutable-映?)
 (define 映/空 (make-immutable-映))
-(define 映/改 dict-set)
-(define 映/取 dict-ref)
-(define 映/删 dict-remove)
-(define 映/含？ dict-has-key?)
+(define 映.增-改 dict-set)
+(define 映.取 dict-ref)
+(define 映.删 dict-remove)
+(define 映.含？ dict-has-key?)
+(define 映→列 dict->list)
 
-(define-record-type 機
-  (機+境 境 形 物)
-  機？
-  (形 機/形)
-  (物 機/物-無-境))
+(define-custom-hash-types mh 等？)
+(define gen/c
+  (let ([c (make-channel)])
+    (λ ()
+      (let ([v (+ 1 (channel-get c))])
+        (channel-put c v)
+        v))))
+(define gen/s (string->symbol (number->string (gen/c))))
+(define h-名/文:sym->_ (make-mutable-映))
+(define h-名/文:_->sym (make-mutable-映))
+(define (名/文 列)
+  (dict-ref
+   h-名/文:_->sym 列
+   (λ ()
+     (let ([s (gen/s)])
+       (dict-set! h-名/文:sym->_ s 列) ; 可能bug： 多處理器時
+       (dict-set! h-名/文:_->sym 列 s)
+       s))))
+(define (名/文？ 甲) (dict-has-key? h-名/文:sym->_ 甲))
+(define (名/文-1 名/文) (dict-ref h-名/文:_->sym 名/文))
+(define h-名/構:sym->pair (make-mutable-映))
+(define h-名/構:pair->sym (make-mutable-映))
+(define (名/構 名 列)
+  (let ([p (cons 名 列)])
+    (dict-ref
+     h-名/構:pair->sym p
+     (λ ()
+       (let ([s (gen/s)])
+         (dict-set! h-名/構:sym->pair s p)
+         (dict-set! h-名/構:pair->sym p s))))))
+(define (名/構？ 甲) (dict-has-key? h-名/構:sym->pair 甲))
+(define (名/構.名 名/構) (car (dict-ref h-名/構:sym->pair 名/構)))
+(define (名/構.列 名/構) (cdr (dict-ref h-名/構:sym->pair 名/構)))
+(define (名？ 甲) (or (名/文？ 甲) (名/構？ 甲)))
+
+(struct 機 (境 形 物))
+(define 機？ 機?)
+(define #%機!境 機-境)
+(define 機.形 機-形)
+(define #%機!物 機-物)
 
 (define 陰 #f)
 (define 楊 #t)
+(define-syntax-rule (#%若 b x y) (if b x y))
 
-(define-record-type 機-引
-  (機-引 機)
-  機-引？
-  (機 機-引/反))
-(define-record-type 機-譯
-  (機-譯 機)
-  機-譯？
-  (機 機-譯/反))
+(struct 引-機 (機))
+(define 引-機？ 引-機?)
+(define 引-機-1 引-機-機)
 
-(define-record-type 誤
-  (誤 甲)
-  誤？
-  (甲 誤/反))
+(struct 誤 (物))
+(define 誤？ 誤?)
+(define 誤-1 誤-物)
 
-(define-record-type 構
-  (構 名 列)
-  構？
-  (名 構/名)
-  (列 構/列))
+(struct 構 (名 列))
+(define 構？ 構?)
+(define 構.名 構-名)
+(define 構.列 構-列)
