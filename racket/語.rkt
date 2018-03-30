3f59569f1eb37d9376eba25d6605e938b25749d7
zaoqi <zaomir@outlook.com>
2018-03-30 21:26:48 +0800

    .

--- racket/語.rkt
+++ racket/語.rkt
@@ -14,3 +14,36 @@
 ;;    You should have received a copy of the GNU Affero General Public License
 ;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #lang racket
+{require (file "構.rkt")}
+{define WIP (delay (error))}
+{define (undelay x f)
+  {cond
+    [(promise? x)
+     (if (promise-forced? x)
+         (undelay (force x) f)
+         (delay (undelay (force x) f)))]
+    [else (f x)]}}
+{define-syntax APP
+  {syntax-rules ()
+    [{_ e r () ()} r]
+    [{_ e r ((p? x0) x ...) (v0 v ...)}
+     (undelay v0
+              {λ (x0)
+                (if (p? x0)
+                    {APP e r (x ...) (v ...)}
+                    e)})]
+    [{_ e r (x0 x ...) (v0 v ...)}
+     {let ([x0 v0])
+       {APP e r (x ...) (v ...)}}]}}
+{define-syntax 定/機%1
+  {syntax-rules ()
+    [{_ (p? x0)} x0]
+    [{_ x0} x0]}}
+{define-syntax-rule {定/機%2 形 ...} ({定/機%1 形} ...)}
+{define-syntax-rule {定/機 (名 形 ...) 物}
+  {define 名
+    (#%機-內
+     {λ {定/機%2 形 ...}
+       {APP WIP 物 (形 ...) {定/機%2 形 ...}}}
+     {quote {定/機%2 形 ...}}
+     WIP)}}
