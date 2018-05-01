be0bb716d0e6804c78c9f89098e53abd153ec162
zaoqi <zaomir@outlook.com>
2018-05-01 20:31:35 +0800

    racket init

--- 化/racket/info.rkt
+++ 化/racket/info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "lang")
-(define deps '("racket"))
+(define deps '("base" "lazy"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("lang.scrbl" ())))
 (define pkg-desc "Description Here")
