6b7c68a8417af9ff881667641e3d8de6aa73d2ca
zaoqi <zaomir@outlook.com>
2018-05-01 20:17:39 +0800

    racket init

---
+++ 化/README.md
@@ -0,0 +1,1 @@
+../DOC/lambda.md
---
+++ 化/racket/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "lang")
+(define deps '("racket"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("lang.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(zaoqi))
