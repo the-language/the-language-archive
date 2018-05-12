02342195e0598a9ce8c8d1cf74346b51a16f7513
zaoqi <zaomir@outlook.com>
2018-05-12 21:59:02 +0800

    .

--- 化/racket/名.rkt
+++ 化/racket/名.rkt
@@ -6,10 +6,11 @@
 {define list→名
   {match-lambda
     [(list #\# #\% 甲 ..1) (→名/構 '式 (list (list→名 甲)))]
+    [(list 甲 ..1 (or #\？ #\?)) "WIP"]
     [(list 甲 ..1 #\: 乙 ..1) (→名/構 '一 (list (list→type 乙) (list→名 甲)))]
     [(list #\: 甲 ..1) (→名/構 '一 (list (list→type 甲)))]
-    [(list 甲 ..1 #\→ 乙 ..1) ("WIP" (list→type 甲) (list→type 乙))]
-    [(list #\→ 甲 ..1) ("WIP" (list→type 甲))]
+    [(or (list 甲 ..1 #\→ 乙 ..1) (list 甲 ..1 #\- #\> 乙 ..1)) ("WIP" (list→type 甲) (list→type 乙))]
+    [(or (list #\→ 甲 ..1) (list #\- #\> 甲 ..1)) ("WIP" (list→type 甲))]
     }}
 {define list→type
   {match-lambda
