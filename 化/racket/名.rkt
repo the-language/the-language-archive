6e92004c701d595f1b7928b9f1c96349fd0b447b
zaoqi <zaomir@outlook.com>
2018-05-13 13:38:13 +0800

    .

--- 化/racket/名.rkt
+++ 化/racket/名.rkt
@@ -14,7 +14,18 @@
     [(or (list 甲 ..1 #\→) (list 甲 ..1 #\- #\>)) ("WIP" (list→type 甲))]
     }}
 {define list→type
-  {match-lambda
-    [(list #\陰) (→構 (→名/構 '子 (list '類 '陰)) '())]
-    }}
+  ({λ ()
+     {define 其:type (→構 (→名/構 '子 '(類 其) '()))}
+     {define 陰 (→構 (→名/構 '子 '(類 陰)) '())}
+     {define 陽 (→構 (→名/構 '子 '(類 陽)) '())}
+     {define 連 (→構 (→名/構 '子 '(類 列 連)) (list 其:type 其:type))}
+     {define 空 (→構 (→名/構 '子 '(類 列 空)) '())}
+     {define list→type
+       {match-lambda
+         [(list #\陰) 陰]
+         [(list #\陽) 陽]
+         [(list #\列 #\/ #\連) 連]
+         [(list #\列 #\/ #\空) 空]
+         }}
+     list→type})}
 {define-syntax-rule {引 x} (symbol→名 'x)}
