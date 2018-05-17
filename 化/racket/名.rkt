bfc8b1860ebb255d9890922788aa47a0bed555c3
zaoqi <zaomir@outlook.com>
2018-05-17 18:12:00 +0800

    .

--- 化/racket/名.rkt
+++ 化/racket/名.rkt
@@ -3,7 +3,9 @@
 {provide 引}
 
 {define 其:type (→構 (→名/構 '子 '(類 其)) '())}
-{define 化類名 (→名/構 '子 '(類 化))}
+{define 化:類名 (→名/構 '子 '(類 化))}
+{define 連:類名 (→名/構 '子 '(類 列 連))}
+     {define 空:類 (→構 (→名/構 '子 '(類 列 空)) '())}
 {define (symbol→名 sym) (list→名 (string->list (symbol->string sym)))}
 {define list→名
   {match-lambda
@@ -13,11 +15,11 @@
     [(list #\: 甲 ..1) (→名/構 '一 (list (list→type 甲)))]
     [(or (list 甲 ..1 #\→ 乙 ..1) (list 甲 ..1 #\- #\> 乙 ..1))
      (→名/構 '一 (list
-               (→構 化類名 (list (list→type 甲) (list→type 乙)))
+               (→構 化:類名 (list (→構 連:類名 (list (list→type 甲) 空:類)) (list→type 乙)))
                '其))]
     [(or (list #\→ 甲 ..1) (list #\- #\> 甲 ..1))
      (→名/構 '一 (list
-               (→構 化類名 (list 其:type (list→type 甲)))
+               (→構 化:類名 (list 其:type (list→type 甲)))
                '其))]
     [(or (list 甲 ..1 #\→) (list 甲 ..1 #\- #\>)) ("WIP" (list→type 甲))]
     [甲 (string->symbol (list->string 甲))]
@@ -28,14 +30,13 @@
      {define 一其 (list 其:type)}
      {define 陰 (→構 (→名/構 '子 '(類 陰)) '())}
      {define 陽 (→構 (→名/構 '子 '(類 陽)) '())}
-     {define 連 (→構 (→名/構 '子 '(類 列 連)) 二其)}
-     {define 空 (→構 (→名/構 '子 '(類 列 空)) '())}
+     {define 連 (→構 連:類名 二其)}
      {define 文 (→構 (→名/構 '子 '(類 名 文)) '())}
      {define 名/構 (→構 (→名/構 '子 '(類 名 構)) 二其)}
      {define 物 (→構 (→名/構 '子 '(類 物)) '())}
      {define 表 (→構 (→名/構 '子 '(類 表)) 二其)}
      {define 集 (→構 (→名/構 '子 '(類 表)) 一其)}
-     {define 化 (→構 化類名 二其)}
+     {define 化 (→構 化:類名 二其)}
      {define 式 (→構 (→名/構 '子 '(類 式)) 一其)}
      {define 構 (→構 (→名/構 '子 '(類 構)) 二其)}
      {define 誤 (→構 (→名/構 '子 '(類 誤)) 一其)}
@@ -45,7 +46,7 @@
          ['(#\陰) 陰]
          ['(#\陽) 陽]
          ['(#\列 #\/ #\連) 連]
-         ['(#\列 #\/ #\空) 空]
+         ['(#\列 #\/ #\空) 空:類]
          ['(#\名 #\/ #\文) 文]
          ['(#\名 #\/ #\構) 名/構]
          ['(#\表) 表]
