fc62da35d73bceb0900c897618991476ac83e696
zaoqi <zaomir@outlook.com>
2018-05-27 13:46:28 +0800

    .

--- 化/racket/名.rkt
+++ 化/racket/名.rkt
@@ -1,2 +1,9 @@
 #lang racket
 {require (file "cons.rkt")}
+
+{define list->名
+  {match-lambda
+    [(list #\# #\% 甲 ...) (名.構.建+ '式 `(,(list->名 甲)))]
+    ['WIP 'WIP]
+    [甲 (string->symbol (list->string 甲))]
+    }}
