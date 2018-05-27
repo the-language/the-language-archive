4ae6ed76d542452a479ac1e650031f81f25db578
zaoqi <zaomir@outlook.com>
2018-05-27 13:31:38 +0800

    .

--- 化/racket/構.rkt
+++ 化/racket/構.rkt
@@ -4,6 +4,7 @@
  <陰，陽>.？ <陰，陽>.陰.？ <陰，陽>.陰.其 <陰，陽>.陽.？ <陰，陽>.陽.其 <陰，陽>.若+
  列.？ 列.連.？ 列.連.建+ 列.連.首+ 列.連.尾+ 列.空.？ 列.空.其
  名.？ 名.文.？ 名.構.？ 名.構.建+ 名.構.:名+ 名.構.:列+
+ 表.？ 表.空 表.增+ 表.改+ 表.取+ 表.含？ 表.删+ 表.→列
  }
 {module rec racket
   {provide define-rec}
@@ -44,3 +45,13 @@
 {define (名.？ x) (or (名.文.？ x) (名.構.？ x))}
 {define 名.文.？ symbol?}
 {define-rec 名.構.？ (名.構.建+ 名.構.:名+ 名.構.:列+)}
+
+{define-rec 表.？ (#%表 #%un表)}
+;{define 表.空 (#%表 (make-immutable-hash-lazy))}
+{define 表.空 (#%表 '())}
+{define (表.增+ :表 名 :物) (#%表 (dict-set (#%un表 :表) 名 :物))}
+{define (表.改+ :表 名 :化) (#%表 (dict-update (#%un表 :表) 名 :化))}
+{define (表.取+ :表 名) (dict-ref (#%un表 :表) 名)}
+{define (表.含？ :表 名) (dict-has-key? (#%un表 :表) 名)}
+{define (表.删+ :表 名) (dict-remove (#%un表 :表) 名)}
+{define (表.→列 :表) (map (λ (p) (list (car p) (cdr p))) (dict->list (#%un表 :表)))}
