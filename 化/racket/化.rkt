349b91afc98b35314ae046890d0b5749b6b26661
zaoqi <zaomir@outlook.com>
2018-06-02 22:30:36 +0800

    .

--- 化/racket/化.rkt
+++ 化/racket/化.rkt
@@ -27,13 +27,13 @@
 {define 等? equal?}
 
 {define :<S集_S陰_S陽>? boolean?}
-{define (:S陰? 甲) (等? 甲 其:S陰)}
+{define (:S陰? :S物) (等? :S物 其:S陰)}
 {define 其:S陰 #f}
-{define (:S陽? 甲) (等? 甲 其:S陰)}
+{define (:S陽? :S物) (等? :S物 其:S陰)}
 {define 其:S陽 #t}
 {define <S集_S陰_S陽>.若 if}
 
-{define (:S列? 甲) (or (:S列/連? 甲) (:S列/空? 甲))}
+{define (:S列? :S物) (or (:S列/連? :S物) (:S列/空? :S物))}
 {define :S列/連? pair?}
 {define ->S列/連 cons}
 {define S列/連.首 car}
@@ -41,35 +41,35 @@
 {define :S列/空? null?}
 {define 其:S列/空 '()}
 
-{define (:S名? 甲) (or (:S名/文? 甲) (:S名/構? 甲))}
+{define (:S名? :S物) (or (:S名/文? :S物) (:S名/構? :S物))}
 {define :S名/文? symbol?}
 {define-rec :S名/構? (S名/構 S名/構.:S名 S名/構.:S列)}
-;{define/memroize*/forever (→S名/構 :名 :列) (S名/構 :名 :列)}
+;{define/memroize*/forever (→S名/構 :S名 :S列) (S名/構 :S名 :S列)}
 {define ->S名/構 S名/構}
 
 {define-rec :S表? (S表 S表->)}
 ;{define 空:S表 (S表 (make-immutable-hash-lazy))}
 {define 空:S表 (S表 '())}
-{define (S表.增 :表 名 :物) (S表 (dict-set (S表-> :表) 名 :物))}
-{define (S表.改 :表 名 :化) (S表 (dict-update (S表-> :表) 名 :化))}
-{define (S表.取 :表 名) (dict-ref (S表-> :表) 名)}
-{define (S表.含？ :表 名) (dict-has-key? (S表-> :表) 名)}
-{define (S表.删 :表 名) (dict-remove (S表-> :表) 名)}
-{define (S表->S列 :表) (map {入 (p) (list (car p) (cdr p))} (dict->list (S表-> :表)))}
+{define (S表.增 :S表 名 :S物) (S表 (dict-set (S表-> :S表) 名 :S物))}
+{define (S表.改 :S表 名 :S化) (S表 (dict-update (S表-> :S表) 名 :S化))}
+{define (S表.取 :S表 名) (dict-ref (S表-> :S表) 名)}
+{define (S表.含？ :S表 名) (dict-has-key? (S表-> :S表) 名)}
+{define (S表.删 :S表 名) (dict-remove (S表-> :S表) 名)}
+{define (S表->S列 :S表) (map {入 (p) (list (car p) (cdr p))} (dict->list (S表-> :S表)))}
 
 {define-rec :S集? (S集 S集->)}
-{define 空:集 (S集 空:S表)}
-{define (S集.增 :集 :物) (S集 (S表.增 (S集-> :集) :物 #t))}
-{define (S集.含？ :集 :物) (S表.含？ (S集-> :集) :物)}
-{define (S集.删 :集 :物) (S集 (S表.删 (S集-> :集) :物))}
-{define (S集->S列 :集) (map S列/連.首 (S表->S列 (S集-> :集)))}
+{define 空:S集 (S集 空:S表)}
+{define (S集.增 :S集 :S物) (S集 (S表.增 (S集-> :S集) :S物 #t))}
+{define (S集.含？ :S集 :S物) (S表.含？ (S集-> :S集) :S物)}
+{define (S集.删 :S集 :S物) (S集 (S表.删 (S集-> :S集) :S物))}
+{define (S集->S列 :S集) (map S列/連.首 (S表->S列 (S集-> :S集)))}
 {define (集 . xs)
   (if (null? xs)
-      空:集
+      空:S集
       (S集.增 (apply 集 (cdr xs)) (car xs)))}
 
 {define-rec :S化? (S化 #%S化.內 S化.形 S化.:S物)}
-{define (->S化 形 :物) (S化 #f 形 :物)}
+{define (->S化 形 :S物) (S化 #f 形 :S物)}
 
 {define-rec :S式? (->S式 S式->)}
 {define-rec :S構? (->S構 S構.:S名 S構.:S列)}
