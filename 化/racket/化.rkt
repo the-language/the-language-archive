bafd5c099995c53eb7992f3729a20d2fadc76cc0
zaoqi <zaomir@outlook.com>
2018-06-07 19:22:38 +0800

    .

--- 化/racket/化.rkt
+++ 化/racket/化.rkt
@@ -121,20 +121,22 @@
           (if (S表.含？ 表s 物)
               (k 名s (S表.删 表s 物) (string-append "周("(S表.取 表s 物)" "str")"))
               (k 名s 表s str))}
+        {define (二 f1 f2 f3)
+          (%示 名s n物s 表s (f1 物)
+               {入 (名s 表s str甲)
+                  (%示 名s n物s 表s (f2 物)
+                      {入 (名s 表s str乙)
+                         (R 名s 表s (f3 str甲 str乙))})})}
         {define n物s (S集.增 物s 物)}
         {cond
           [(S集.含？ 物s 物)
            {let ([a (symbol->string (car 名s))])
-                          (k (cdr 名s) (S表.增 表s 物 a) (string-append "周"a))}]
+             (k (cdr 名s) (S表.增 表s 物 a) (string-append "周"a))}]
           [(:S陰? 物) (k 名s 表s "陰")]
           [(:S陽? 物) (k 名s 表s "陽")]
-          [(:S列/連? 物)
-           (%示 名s n物s 表s (S列/連.首 物)
-               {入 (名s 表s str甲)
-                  (%示 名s n物s 表s (S列/連.尾 物)
-                      {入 (名s 表s str乙)
-                         (R 名s 表s (string-append "連("str甲" "str乙")"))})})]
+          [(:S列/連? 物) (二 S列/連.首 S列/連.尾 {入 (甲 乙) (string-append "連("甲" "乙")")})]
           [(:S名/文? 物) (k 名s 表s (string-append "文|"(symbol->string 物)"|"))]
+          [(:S名/構? 物) (二 S名/構.:S名 S名/構.:S列 {入 (甲 乙) (string-append "名("甲" "乙")")})]
           [else 'WIP]}
         }
       示
