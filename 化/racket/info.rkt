f7f94a15401ed852073f6d28b1046641a37fd2f4
zaoqi <zaomir@outlook.com>
2018-06-29 14:57:46 +0800

    重init

--- 化/racket/info.rkt
+++
@@ -1,8 +0,0 @@
-#lang info
-(define collection "lang")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/lang.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(zaoqi))
