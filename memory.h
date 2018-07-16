6f83886abaa09e3222a45c8f905932d040cd5eb7
zaoqi <zaomir@outlook.com>
2018-07-16 17:15:20 +0800

    .

--- memory.h
+++ memory.h
@@ -15,8 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef _HEAD_memory_
-#define _HEAD_memory_
+#ifndef memory_new_type
 
 
 #include "c.h"
